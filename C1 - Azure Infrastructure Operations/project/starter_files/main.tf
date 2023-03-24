3e06ff0eab5353fda6b0c326218fde0d1caf7f60
brem02 <122722304+brem02@users.noreply.github.com>
2023-03-24 22:14:59 +0100

    Update main.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -54,8 +54,8 @@ resource "azurerm_subnet" "internal" {
 }
 
 # Create the network security group
-resource "azurerm_network_security_group" "web_server_nsg" {
-  name                = "web_server_nsg"
+resource "azurerm_network_security_group" "main" {
+  name                = "main"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -171,7 +171,7 @@ resource "azurerm_lb" "main" {
 
 # The load balancer will use this backend pool
 resource "azurerm_lb_backend_address_pool" "main" {
-  resource_group_name = azurerm_resource_group.main.name
+  #resource_group_name = azurerm_resource_group.main.name
   loadbalancer_id     = azurerm_lb.main.id
   name                = "${var.prefix}-lb-backend-address-pool"
 }
@@ -213,7 +213,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
-    create_option        = "Empty"
+    #create_option        = "Empty"
     disk_size_gb         = "1"
   }
 
