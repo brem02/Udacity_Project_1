7b88ddd011f38c2a35d4d1d857c7d94b409481bf
brem02 <122722304+brem02@users.noreply.github.com>
2023-04-22 11:12:38 +0200

    Update main.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -233,7 +233,7 @@ resource "azurerm_managed_disk" "main" {
   resource_group_name             = data.azurerm_resource_group.main.name
   storage_account_type            = "Standard_LRS"
   create_option                   = "Empty"
-  #disk_size_gb                    = 1
+  disk_size_gb                    = 1
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "main" {
