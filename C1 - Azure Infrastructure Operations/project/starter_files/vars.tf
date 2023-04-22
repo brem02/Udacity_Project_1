199f7c3469dbd27116882d7f237902ed910888b9
brem02 <122722304+brem02@users.noreply.github.com>
2023-04-22 12:11:19 +0200

    Update vars.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/vars.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/vars.tf
@@ -12,7 +12,7 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default     = "West Europe"
+  default     = "South Central US"
 }
 
 variable "username" {
